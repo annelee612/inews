ee55d65e4befb611756a0440577867847720885e
Tom Grek <tom.grek@gmail.com>
2016-11-01 14:57:55 -0700

    Implement newsfeed create and fetch at backend

--- server/models/Feed.js
+++ server/models/Feed.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
 const feedSchema = mongoose.Schema({
+  id: String, // a short id for the feed
   title: String,
   color: String,
   order: Number,
   newsItems: Array,
-  userId: String
+  userId: String,
+  lastRefresh: { type: Date, default: 0 }
 });
+
+// get updated news from Bing
+feedSchema.methods.refresh = function() {
+  return new Promise((resolve, reject) => {
+    this.newsItems.push({title:'A news item'});
+    this.save().then(() => resolve(this));
+  });
+}
 
 var Feed = mongoose.model('Feed', feedSchema);
 
--- server/routers/getFeed.js
+++ server/routers/getFeed.js
@@ -1,12 +1,36 @@
 var express = require('express');
+var Feed = require('../models/Feed.js');
 
 //local files
 var API_KEY = require('../config.js').API_KEY;
 
 var router = express.Router();
 
+router.route('/').post(function(req, res) {
+  let newFeed = new Feed();
+  newFeed.title = req.body.title;
+  newFeed.userId = req.user ? req.user.username : 't'; // default username for testing only
+  newFeed.color = '#3366ff';
+  newFeed.order = 1;
+  newFeed.id = newFeed.title.toLowerCase().replace(/ /g, '-');
+  newFeed.save().then(function(feed) {
+    feed.refresh().then(fullFeed => res.json(fullFeed));
+  });
+});
+
+router.route('/:id').get(function(req, res) {
+  Feed.findOne({id: req.params.id}).then(function(feed) {
+    if (!feed) return res.json({message: 'No such feed'});
+    return res.json(feed);
+  });
+});
+
 router.route('/').get(function(req, res) {
-  res.json({feeds: ['Election', 'Liverpool', 'Tentacles', 'Robot Consciousness']});
+  if (req.user) {
+    res.json({feeds: ['Pugs', 'Catholic Priests', 'React vs Angular', 'Hack Reactor']});
+  } else {
+    res.json({feeds: ['Election', 'Liverpool', 'Tentacles', 'Robot Consciousness']});
+  }
 });
 
 module.exports = router;
