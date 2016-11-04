775454404d2fc771096b98932c5fe481a50d1e19
Robin Gulbrandsen <robin@123.no>
2016-11-03 19:49:03 -0700

    moved files after merge

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ var config = {
   API_KEY : 'a54facec813042d9aad23b1fef21141b',
   MEETUP_API_KEY: '2b5d7445522d554d7767644875407549',
   MONGODB_LOCAL_URI: 'mongodb://localhost/inews',
-  MONGODB_STAGING_URI: 'mongodb://devMongo:27017/inews'
+  MONGODB_STAGING_URI: 'mongodb://localhost/inews' //'mongodb://devMongo:27017/inews'
 }
 
 module.exports = config;
